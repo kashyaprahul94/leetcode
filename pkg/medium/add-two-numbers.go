8d3b48d0813fa5458e9e973ea257fed2235fee7e
Rahul Kashyap <kashyaprahul94@gmail.com>
2021-01-04 16:58:45 +0530

    Adding the correct implementation for add-two-numbers

--- pkg/medium/add-two-numbers.go
+++ pkg/medium/add-two-numbers.go
@@ -1,5 +1,80 @@
 package medium
 
-func addTwoNumbers (one, two int) int {
-	return one + two
+import "fmt"
+
+// ListNode represents a singly linked list
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func (list *ListNode) String() string {
+	result := "Start"
+	head := list
+
+	for head != nil {
+		result = fmt.Sprintf("%s -> %v", result, head.Val)
+		head = head.Next
+	}
+
+	return fmt.Sprintf("%s -> End", result)
+}
+
+func (list *ListNode) toArray() []int {
+	var result []int
+
+	head := list
+
+	for head != nil {
+		result = append(result, head.Val)
+		head = head.Next
+	}
+
+	return result
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	result := &ListNode{
+		Val: 0,
+	}
+	current := result
+
+	carry := 0
+
+	for l1 != nil || l2 != nil {
+
+		x, y, sum := 0, 0, 0
+
+		if l1 != nil {
+			x = l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			y = l2.Val
+			l2 = l2.Next
+		}
+
+		sum = x + y + carry
+
+		if sum >= 10 {
+			sum = sum - 10
+			carry = 1
+		} else {
+			carry = 0
+		}
+
+		current.Next = &ListNode{
+			Val: sum,
+		}
+		current = current.Next
+	}
+
+	if carry != 0 {
+		current.Next = &ListNode{
+			Val: carry,
+		}
+	}
+
+	return result.Next
 }
--- pkg/medium/add-two-numbers_test.go
+++ pkg/medium/add-two-numbers_test.go
@@ -1,11 +1,127 @@
 package medium
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestAddTwoNumbers (t *testing.T) {
-	res := addTwoNumbers(1, 2)
+func TestAddTwoNumbers(t *testing.T) {
 
-	if res != 3 {
-		t.Fatalf("Expected %v but got %v", 3, res)
+	testCases := []struct {
+		name string
+		l1   *ListNode
+		l2   *ListNode
+		want []int
+	}{
+		{
+			name: "Case 1",
+			l1: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 3,
+					},
+				},
+			},
+			l2: &ListNode{
+				Val: 5,
+				Next: &ListNode{
+					Val: 6,
+					Next: &ListNode{
+						Val: 4,
+					},
+				},
+			},
+			want: []int{7, 0, 8},
+		},
+		{
+			name: "Case 2",
+			l1: &ListNode{
+				Val: 9,
+				Next: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+						Next: &ListNode{
+							Val: 9,
+							Next: &ListNode{
+								Val: 9,
+								Next: &ListNode{
+									Val: 9,
+									Next: &ListNode{
+										Val: 9,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			l2: &ListNode{
+				Val: 9,
+				Next: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+						Next: &ListNode{
+							Val: 9,
+						},
+					},
+				},
+			},
+			want: []int{8, 9, 9, 9, 0, 0, 0, 1},
+		},
+		{
+			name: "Case 3",
+			l1: &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 1,
+				},
+			},
+			l2: &ListNode{
+				Val: 9,
+				Next: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+					},
+				},
+			},
+			want: []int{0, 1, 0, 1},
+		},
+		{
+			name: "Case 4",
+			l1: &ListNode{
+				Val: 0,
+				Next: &ListNode{
+					Val: 0,
+					Next: &ListNode{
+						Val: 1,
+					},
+				},
+			},
+			l2: &ListNode{
+				Val: 9,
+			},
+			want: []int{9, 0, 1},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			got := addTwoNumbers(testCase.l1, testCase.l2)
+
+			if len(got.toArray()) != len(testCase.want) {
+				t.Fatalf("%v failed. Expected %v, got %v", testCase.name, testCase.want, got.toArray())
+			}
+
+			for i, v := range got.toArray() {
+				if v != testCase.want[i] {
+					t.Fatalf("%v failed. Expected %v, got %v", testCase.name, testCase.want, got.toArray())
+					break
+				}
+			}
+		})
 	}
 }
